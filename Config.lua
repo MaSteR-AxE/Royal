fca2ab1bd937b7a61ebf028f92b063b8ddf6659b
MaSteR-AxE <41484361+MaSteR-AxE@users.noreply.github.com>
2018-12-12 13:11:57 +0330

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,7 +1,8 @@
 do
 local B = {
-SudoID = "591735330",
-Channel = "🦋 @SkyNetTM 🌹",
+SudoID = "143702561",   --Your ID, For getting your id send a message to this bot --> @userinfoBOT
+Channel = "🦋 @SkyNetTM 🌹",   --Your channel Address, For example : @MaSteR_AxE
+ID = "@XXX",   --Your UserName, For example : @Ma5t3R_AxE
 		}
 return B
 end
