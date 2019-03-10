89463ca25b700ad4a6ca9c43484e9a425928a664
MaSteR-AxE <41484361+MaSteR-AxE@users.noreply.github.com>
2019-03-10 21:10:09 +0330

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,8 +1,8 @@
 do
 local B = {
 SudoID = "591735330",   --Your ID, For getting your id send a message to this bot --> @userinfoBOT
-Channel = "🦋 @SkyNetTM 🌹",   --Your channel Address, For example : @MaSteR_AxE
-USERNAME = "@XXX",   --Your UserName, For example : @Ma5t3R_AxE
+Channel = "🦋 @MaSteR_AxE 🌹",   --Your channel Address, For example : @MaSteR_AxE
+USERNAME = "@Ma5t3R_AxE",   --Your UserName, For example : @Ma5t3R_AxE
 		}
 return B
 end
